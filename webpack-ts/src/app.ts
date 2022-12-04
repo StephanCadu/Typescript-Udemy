ec4b3af8c4ad2c1636e98824378815491dc74a5b
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2022-12-04 20:17:11 -0300

    📚 configuring webpack server

--- webpack-ts/src/app.ts
+++ webpack-ts/src/app.ts
@@ -4,5 +4,5 @@ import $ from 'jquery'
 const book = new Book('Dom Quixote', 108.80, 0.1)
 // console.log(book.discountedPrice());
 
-$('body').append(`<h1>${book.name}</h1>`)
+$('body').append(`<h1>Nome: ${book.name}</h1>`)
 $('body').append(`<h2>Preço: ${book.discountedPrice()}</h2>`)
--- webpack-ts/webpack.config.js
+++ webpack-ts/webpack.config.js
@@ -4,6 +4,11 @@ const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
   mode: 'production',
   entry: './src/app.ts',
+  devServer: {
+    watchFiles: path.join(__dirname, 'dist'),
+    port: 3002,
+    hot: true,
+  },
   output: {
     filename: 'app.min.js',
     path: path.join(__dirname, 'dist')
