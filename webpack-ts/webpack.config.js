2469977d965f24af160c745da6e07053fc8c7375
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2022-12-04 20:09:38 -0300

    📚 copying app.min.js to ./public using copy-webpack-plugin

--- webpack-ts/webpack.config.js
+++ webpack-ts/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'production',
@@ -7,6 +8,11 @@ module.exports = {
     filename: 'app.min.js',
     path: path.join(__dirname, 'dist')
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
   resolve: {
     extensions: ['.ts', '.js']
   },
@@ -18,3 +24,4 @@ module.exports = {
     }]
   }
 }
+
