6de3380f7ed53b4db36e4d943e99860f492333ac
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2022-12-04 21:34:15 -0300

    📚 creating vue + typescript config and Counter component (with ts error)

---
+++ vue-ts/src/components/MyCounter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Contador:</h1>
+    <h2>{{ value }}</h2>
+    <button @click="setValue(1)">+</button>
+    <button @click="setValue(-1)">-</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    initialValue: {
+      type: Number
+    }
+  },
+  data() {
+    return {
+      value: this.initialValue || 0
+    }
+  },
+  methods: {
+    setValue(delta: number) {
+      this.value += delta
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
